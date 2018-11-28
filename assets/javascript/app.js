d50eea5a424d3b71fb3d7f1adad6a302f2077a76
Scripted Well <whigareda@theblackpilledpage.com>
2018-11-28 05:35:18 -0700

    Final Draft - updated HTML, CSS, JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,90 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-    //Button variables
-    var displayButtons = ["Independence Day", "Veteran/'s Day", "Halloween", "Thanksgiving", "Christmas"];
+    var displayedButtons = ["Independence Day", "Veteran's Day", "Halloween", "Thanksgiving", "Christmas"];
 
-    //Function to display images
-    function displayImages() {
-        $("#images").empty();
+    function displayImg(){
+
+        $("#display-images").empty();
         var input = $(this).attr("data-name");
         var limit = 10;
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=UQQLAcG6Z0ggomudLwifhq9Nhtym5Tpo";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=UQQLAcG6Z0ggomudLwifhq9Nhtym5Tpo";   
 
-    //ajax method
-        $.ajax ({
-            url: queryURL,
+        $.ajax({
+            url: queryURL, 
             method: "GET"
         }).done(function(response) {
 
-            //for loop for display, rating, and images
-            for(var j = 0; j < limit; j++) {
+            for(var j = 0; j < limit; j++) {    
 
                 var displayDiv = $("<div>");
                 displayDiv.addClass("holder");
-
+            
                 var image = $("<img>");
-                image.attr("src", response.data[j].images.orignal_still.url);
-                image.attr("data-still", response.data[j].images.orignal_still.url);
-                image.attr("data-animate", response.data[j].images.orignal_still.url);
+                image.attr("src", response.data[j].images.original_still.url);
+                image.attr("data-still", response.data[j].images.original_still.url);
+                image.attr("data-animate", response.data[j].images.original.url);
                 image.attr("data-state", "still");
                 image.attr("class", "gif");
                 displayDiv.append(image);
 
                 var rating = response.data[j].rating;
                 console.log(response);
-                var pRating = $("<p>").html("Rating: " + rating);
+                var pRating = $("<p>").text("Rating: " + rating);
                 displayDiv.append(pRating)
 
-                $("#images").append(displayDiv);
+                $("#display-images").append(displayDiv);
             }
         });
     }
 
-    //Function renderButtons
+    function renderButtons(){ 
 
-    //Function gifStateChange
+        $("#display-buttons").empty();
 
+        for (var i = 0; i < displayedButtons.length; i++){
 
+            var newButton = $("<button>") 
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", displayedButtons[i]); 
+            newButton.text(displayedButtons[i]); 
+            $("#display-buttons").append(newButton); 
+        }
+    }
 
+    function imageChangeState() {          
 
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
+
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
+        }   
+    }
+
+    $("#submitPress").on("click", function(){
+
+        var input = $("#user-input").val().trim();
+        form.reset();
+        displayedButtons.push(input);
+                
+        renderButtons();
+
+        return false;
+    })
+
+    renderButtons();
+
+    $(document).on("click", "#input", displayImg);
+    $(document).on("click", ".gif", imageChangeState);
+});
 
 
 
@@ -56,4 +94,3 @@ $(document).ready(function() {
 
 
     
-});
